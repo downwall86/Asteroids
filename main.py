82b04e8bf54a8302328f2fb34cd0e332de614cfd
downwall86 <rightdownwall86@outlook.com>
2025-04-10 13:30:37 -0400

    drawing the player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,16 +10,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    playerone = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     stillrunning = 1
     clock = pygame.time.Clock()
     dt = 0
     while stillrunning == 1:
         screen.fill((0, 0, 0))
+        playerone.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         dt = (clock.tick(60)) / 1000
+      
 
 
 
